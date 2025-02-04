5aff584a36064f0c08ae888bd9f77e9af20548a2
barcaca <73846225+barcaca@users.noreply.github.com>
2025-02-04 16:03:52 -0300

    feat(pages): Criação da página do challenge QR Code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import { Icons } from '@/components/icons'
-import { buttonVariants } from '@/components/ui/button'
+import { Footer } from '@/components/footer'
 import Link from 'next/link'
 
-const links = [{ href: '', label: '' }]
+const links = [{ href: '1-qr-code', label: 'Challenge 1 - QR code component' }]
 
 export default function Home() {
   return (
@@ -23,41 +22,5 @@ export default function Home() {
       </header>
       <Footer />
     </div>
-  )
-}
-
-const socials = [
-  { name: 'GitHub', url: 'https://github.com/barcaca', icon: Icons.gitHub },
-  {
-    name: 'LinkedIn',
-    url: 'https://linkedin.com/in/luan-barcaça/',
-    icon: Icons.linkedin,
-  },
-  { name: 'Email', url: 'mailto:luan.barcaca@gmail.com', icon: Icons.email },
-]
-
-export function Footer() {
-  const year = new Date().getFullYear()
-  return (
-    <footer className="flex w-full flex-col items-center justify-center gap-4 py-8 md:flex-row md:justify-between">
-      <p className="order-2 font-semibold text-muted-foreground text-sm md:order-1">
-        © {year} Luan, All Rights Reserved
-      </p>
-      <div className="order-1 inline-flex items-center gap-8 md:order-2">
-        {socials.map(social => {
-          return (
-            <Link
-              key={social.name}
-              href={social.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={buttonVariants({ variant: 'outline', size: 'icon' })}
-            >
-              <social.icon className="size-6" />
-            </Link>
-          )
-        })}
-      </div>
-    </footer>
   )
 }
