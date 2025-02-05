3047e5b033ba5c34c934de25bd32370b9542fc8e
barcaca <73846225+barcaca@users.noreply.github.com>
2025-02-05 15:47:24 -0300

    feat(components/blog-preview): Adição de animações

--- app/(challenge)/2-blog-preview/page.tsx
+++ app/(challenge)/2-blog-preview/page.tsx
@@ -1,8 +1,17 @@
 import imageCard from '@/assets/2-blog-preview/illustration-article.svg'
 import avatar from '@/assets/2-blog-preview/image-avatar.webp'
 import { Footer } from '@/components/footer'
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { container, item } from '@/components/shared/_animate'
+import {
+  MotionCard,
+  MotionCardDescription,
+  MotionCardFooter,
+  MotionCardTitle,
+  MotionImage,
+} from '@/components/shared/_components/motion-qr-code'
+import { CardHeader } from '@/components/ui/card'
 import { figtree } from '@/fonts/font'
+import * as motion from 'motion/react-client'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -10,34 +19,47 @@ const BlogPreviewCardPage = () => {
   return (
     <div className="flex min-h-screen flex-col bg-[#F4D04E] p-7">
       <main className="flex flex-1 items-center justify-center">
-        <Card
+        <MotionCard
+          variants={container}
+          initial={'hidden'}
+          animate={'show'}
+          whileHover={'hover'}
           className={`${figtree.className} w-full max-w-sm space-y-6 border border-black p-6 leading-[150%] shadow-[8px_8px_0_0_rgba(0,0,0,1)] shadow-black`}
         >
-          <Image
+          <MotionImage
+            variants={item.scale}
             src={imageCard}
             alt="Image"
             className="h-[12.5rem] w-full rounded-md object-cover"
           />
-          <CardHeader className="space-y-3 p-0">
-            <span className="w-fit rounded-sm bg-[#F4D04E] object-contain px-3 py-1 font-extrabold text-sm">
+          <CardHeader className="space-y-3 overflow-hidden p-0">
+            <motion.span
+              variants={item.translateYDown}
+              className="w-fit rounded-sm bg-[#F4D04E] object-contain px-3 py-1 font-extrabold text-sm"
+            >
               Learning
-            </span>
-            <span className="font-medium text-sm">Published 21 Dec 2023</span>
+            </motion.span>
+            <motion.span variants={item.opacity} className="font-medium text-sm">
+              Published 21 Dec 2023
+            </motion.span>
             <Link href={'#'}>
-              <CardTitle className="font-extrabold text-2xl transition-colors hover:text-[#F4D04E]">
+              <MotionCardTitle
+                variants={item.translateXRight}
+                className="font-extrabold text-2xl transition-colors hover:text-[#F4D04E]"
+              >
                 HTML & CSS foundations
-              </CardTitle>
+              </MotionCardTitle>
             </Link>
-            <CardDescription className="font-medium text-base">
+            <MotionCardDescription variants={item.translateXLeft} className="font-medium text-base">
               These languages are the backbone of every website, defining structure, content, and
               presentation.
-            </CardDescription>
+            </MotionCardDescription>
           </CardHeader>
-          <CardFooter className="p-0">
+          <MotionCardFooter variants={item.translateYUp} className="overflow-hidden p-0">
             <Image src={avatar} alt="Avatar" className="rounded-full" width={32} height={32} />
             <span className="ml-3 font-extrabold text-sm">Greg Hooper</span>
-          </CardFooter>
-        </Card>
+          </MotionCardFooter>
+        </MotionCard>
       </main>
       <Footer />
     </div>
