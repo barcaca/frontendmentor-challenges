f833fc7a70f43fdcb967e9ce1281bd44732e4172
barcaca <73846225+barcaca@users.noreply.github.com>
2025-02-06 14:51:11 -0300

    feat: Adicao de animações

--- app/(challenge)/3-social-profile/page.tsx
+++ app/(challenge)/3-social-profile/page.tsx
@@ -1,33 +1,67 @@
 import avatar from '@/assets/3-social-profile/avatar-jessica.jpeg'
 import { Footer } from '@/components/footer'
+import { container, item } from '@/components/shared/_animate'
+import {
+  MotionCard,
+  MotionCardContent,
+  MotionCardDescription,
+  MotionCardTitle,
+  MotionImage,
+  MotionLink,
+} from '@/components/shared/_components/motion-qr-code'
 import { buttonVariants } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { CardHeader } from '@/components/ui/card'
 import { inter } from '@/fonts/font'
 import { cn } from '@/lib/utils'
-import Image from 'next/image'
-import Link from 'next/link'
+import * as motion from 'motion/react-client'
 
 const SocialProfilePage = () => {
   return (
     <div className="flex min-h-screen w-full flex-col bg-[#141414] p-6">
       <main className="flex w-full flex-1 items-center justify-center ">
-        <Card
+        <MotionCard
+          variants={container}
+          initial={'hidden'}
+          animate={'show'}
           className={`${inter.className} w-full max-w-sm space-y-6 border-none bg-[#1f1f1f] p-10`}
         >
           <CardHeader className="items-center space-y-0 p-0">
-            <Image className="mb-6 rounded-full" src={avatar} alt="Avatar" width={90} height={90} />
-            <CardTitle className="mb-1 font-semibold text-2xl text-white">Sarah Randall</CardTitle>
-            <CardDescription className="!mb-6 font-semibold text-[#c5f82a] text-sm">
+            <MotionImage
+              variants={item.scale}
+              className="mb-6 rounded-full"
+              src={avatar}
+              alt="Avatar"
+              width={90}
+              height={90}
+            />
+            <MotionCardTitle
+              variants={item.translateXLeft}
+              className="mb-1 font-semibold text-2xl text-white"
+            >
+              Sarah Randall
+            </MotionCardTitle>
+            <MotionCardDescription
+              variants={item.translateXRight}
+              className="!mb-6 font-semibold text-[#c5f82a] text-sm"
+            >
               London, United Kingdom
-            </CardDescription>
-            <p className="font-medium text-muted-foreground text-sm">
+            </MotionCardDescription>
+            <motion.p
+              variants={item.translateYUp}
+              className="font-medium text-muted-foreground text-sm"
+            >
               “Front-end developer and avid reader.”
-            </p>
+            </motion.p>
           </CardHeader>
-          <CardContent className="flex flex-col gap-4 p-0">
+          <MotionCardContent
+            variants={container}
+            initial={'hidden'}
+            animate={'show'}
+            className="flex flex-col gap-4 p-0"
+          >
             <Socials />
-          </CardContent>
-        </Card>
+          </MotionCardContent>
+        </MotionCard>
       </main>
       <Footer />
     </div>
@@ -36,19 +70,32 @@ const SocialProfilePage = () => {
 
 const socialsLinks = ['Github', 'Frontend Mentor', 'Linkedin', 'Twiiter', 'Instagram']
 
+const linkAnimation = {
+  hidden: { opacity: 0, scale: 0.5 },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+}
+
 const Socials = () => {
-  return socialsLinks.map(social => {
+  return socialsLinks.map(link => {
     return (
-      <Link
-        key={social}
+      <MotionLink
+        variants={linkAnimation}
+        whileHover={{ scale: 1.1 }}
+        key={link}
         href={'#'}
         className={cn(
           buttonVariants({}),
           'h-11 bg-[#333333] hover:bg-[#c5f82a] hover:text-[#141414]'
         )}
       >
-        {social}
-      </Link>
+        {link}
+      </MotionLink>
     )
   })
 }
