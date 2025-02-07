ba15fdd56d183bfe1e46b35779283ea0922e9619
barcaca <73846225+barcaca@users.noreply.github.com>
2025-02-07 16:01:55 -0300

    chore: Adicao de fonte , assets e data para Recipe

---
+++ app/(challenge)/4-recipe/data/data.ts
@@ -0,0 +1,53 @@
+export const preparationItems = [
+  {
+    label: 'Total',
+    value: 'Approximately 10 minutes',
+  },
+  {
+    label: 'Preparation',
+    value: '5 minutes',
+  },
+  {
+    label: 'Cooking',
+    value: '5 minutes',
+  },
+]
+
+export const ingredientsItem = [
+  { value: '2-3 large eggs' },
+  { value: 'Salt, to taste' },
+  { value: 'Pepper, to taste' },
+  { value: '1 tablespoon of butter or oil' },
+  { label: 'Optional fillings', value: 'cheese, diced vegetables, cooked meats, herbs' },
+]
+
+export const instructionsItem = [
+  {
+    label: 'Beat the eggs',
+    value:
+      'In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.',
+  },
+  {
+    label: 'Heat the pan',
+    value: 'Place a non-stick frying pan over medium heat and add butter or oil.',
+  },
+  {
+    label: 'Cook the omelette',
+    value:
+      'Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.',
+  },
+  {
+    label: 'Add fillings (optional)',
+    value:
+      'When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.',
+  },
+  {
+    label: 'Fold and serve',
+    value:
+      'As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.',
+  },
+  {
+    label: 'Enjoy',
+    value: 'Serve hot, with additional salt and pepper if needed.',
+  },
+]
