784312bbdd816aed6164d47b0aa18e0948dec030
barcaca <73846225+barcaca@users.noreply.github.com>
2025-02-18 21:52:10 -0300

    refactor(animations): Refatoração do sistema de animações com classe Builder

--- components/shared/_animate/index.ts
+++ components/shared/_animate/index.ts
@@ -1,115 +1,54 @@
-// Basic animations
+import type { MotionProps, Variant } from 'motion/react'
 
-const opacity = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      duration: 0.8,
-      delay: 1.2,
-      ease: [0, 0.71, 0.2, 1.01],
-    },
-  },
-}
-const translateYUp = {
-  hidden: { opacity: 0, y: 100 },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.8,
-      delay: 0.5,
-      ease: [0, 0.71, 0.2, 1.01],
-    },
-  },
-}
-const translateYDown = {
-  hidden: { opacity: 0, y: -100 },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.8,
-      delay: 0.5,
-      ease: [0, 0.71, 0.2, 1.01],
-    },
-  },
-}
-const translateXLeft = {
-  hidden: { opacity: 0, x: -100 },
-  show: {
-    opacity: 1,
-    x: 0,
-    transition: {
-      duration: 0.8,
-      delay: 0.5,
-      ease: [0, 0.71, 0.2, 1.01],
-    },
-  },
-}
-const translateXRight = {
-  hidden: { opacity: 0, x: 100 },
-  show: {
-    opacity: 1,
-    x: 0,
-    transition: {
-      duration: 0.8,
-      delay: 0.5,
-      ease: [0, 0.71, 0.2, 1.01],
-    },
-  },
-}
-const scale = {
-  hidden: { opacity: 0, scale: 0.5 },
-  show: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.8,
-      delay: 0.5,
-      ease: [0, 0.71, 0.2, 1.01],
-    },
-  },
+class MotionAnimation {
+  constructor(
+    public variants: { [key: string]: Variant } = {},
+    public transition?: MotionProps['transition']
+  ) {}
 }
 
-export const container = {
-  hidden: { opacity: 0, scale: 0.5 },
-  show: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.5,
-      ease: [0, 0.71, 0.2, 1.01],
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-  hover: {
-    scale: [1, 1.1],
-    transition: {
-      duration: 0.2,
-      ease: [0, 0.71, 0.9, 1.01],
-    },
-  },
-}
-export const containerOpacity = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      duration: 0.5,
-      ease: [0, 0.71, 0.2, 1.01],
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
+export class AnimationBuilder {
+  private variants: { [key: string]: Variant } = {}
+  private transition?: MotionProps['transition']
+
+  setInitial(initial: Variant): this {
+    this.variants.initial = initial
+    return this
+  }
+  setAnimate(animate: Variant, transition?: MotionProps['transition']): this {
+    this.variants.animate = {
+      ...animate,
+      transition,
+    }
+    return this
+  }
+  setExit(exit: Variant): this {
+    this.variants.exit = exit
+    return this
+  }
+  setHover(hover: Variant): this {
+    this.variants.hover = hover
+    return this
+  }
+  setTransition(transition: MotionProps['transition']): this {
+    this.transition = transition
+    return this
+  }
+  addVariant(name: string, variant: Variant) {
+    this.variants[name] = variant
+    return this
+  }
+  build() {
+    return new MotionAnimation(this.variants, this.transition)
+  }
 }
 
-export const item = {
-  scale,
-  translateXRight,
-  translateXLeft,
-  translateYDown,
-  opacity,
-  translateYUp,
-}
+/**---------------------- Animations for containers ----------------------**/
+
+const fade = new AnimationBuilder()
+  .setInitial({ opacity: 0 })
+  .setAnimate({ opacity: 1 }, { staggerChildren: 0.2 })
+  .setExit({ opacity: 0 })
+  .build()
+
+/**------------------------ Animations for items ------------------------**/
